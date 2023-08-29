33d154b2fb538e97bf60e43f292572302f2fd289
Parv-gugnani <9765.parv@gmail.com>
2023-08-29 09:20:03 +0530

    spectrogram

---
+++ src/Spectrogram.js
@@ -0,0 +1,9 @@
+import React, { useEffect } from "react";
+import p5 from 'p5';
+
+const Spectrogram = () => {
+    useEffect(() => {
+        const sketch = (p) => {
+            let mic;
+            let fft;)
+}
